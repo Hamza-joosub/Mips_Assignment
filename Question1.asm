fb9e02ac332e36fca014378307ef0d39166f9b45
Hamza-joosub <125697787+Hamza-joosub@users.noreply.github.com>
2023-09-23 21:31:44 +0200

    stack_storage

--- Question1.asm
+++ Question1.asm
@@ -1,7 +1,8 @@
 .data
 filename: .asciiz "jet_64_in_ascii_cr.ppm"  # Replace with your file name
 openError: .asciiz "cant open File"
-buffer: .space 1024
+buffer: .space 1
+
 
 .text
 .globl main
@@ -13,21 +14,28 @@ main:
     li $a1, 0            # Flags: 0 for reading
     syscall
     move $s0, $v0
+    li $s1, 0
 
-    # Check if file open was successful (file descriptor in $v0)
 
     # Read and print lines from the file
 read_loop:
     # Read a character from the file
     li $v0, 14            # System call code for read character
-    move $a0, $s0      # File descriptor in $a0
-    la $a1, buffer        # Buffer address (assuming it's on the stack)
-    li $a2, 2      # Read one character
+    move $a0, $s0         # File descriptor in $a0
+    la $a1, buffer	  # Buffer address (assuming it's on the stack)
+    li $a2, 1      	  # Read one character
+    syscall 
+    beq $v0, $zero, print_loop
+    lb $t0, buffer
+    sb $t0, ($sp)
+    addi $sp, $sp, -1
+    j read_loop
+print_loop:
+    li $v0, 4 
+    la $a0, 0($sp)
+    addi $sp, $sp, 1
     syscall
-
-    li $v0, 4
-    la $a0, buffer
-    syscall
+    
 
     # Check for end of file (EOF)
     #beq $v0, $zero, file_end
