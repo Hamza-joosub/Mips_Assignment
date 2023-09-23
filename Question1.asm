edb6b17ed604001d769049613532a6a45fd9ea0a
Hamza-joosub <125697787+Hamza-joosub@users.noreply.github.com>
2023-09-23 21:31:40 +0200

    Implemented_stack_storage

--- Question1.asm
+++ Question1.asm
@@ -15,7 +15,6 @@ main:
     move $s0, $v0
 
     # Check if file open was successful (file descriptor in $v0)
-    bltz $s0, file_open_error
 
     # Read and print lines from the file
 read_loop:
@@ -23,7 +22,7 @@ read_loop:
     li $v0, 14            # System call code for read character
     move $a0, $s0      # File descriptor in $a0
     la $a1, buffer        # Buffer address (assuming it's on the stack)
-    li $a2, 1024       # Read one character
+    li $a2, 2      # Read one character
     syscall
 
     li $v0, 4
@@ -61,11 +60,6 @@ read_loop:
     #li $v0, 10            # System call code for exit
     #syscall
 
-file_open_error:
-    # Handle file open error (e.g., print an error message)
-    li $v0, 4             # System call code for print string
-    la $a0, openError
-    syscall
 
     # Exit the program
     li $v0, 10            # System call code for exit
